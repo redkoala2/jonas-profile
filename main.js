a38c6e095bcacef78015b136ef2c016f62f33780
Jonas <190408679+redkoala2@users.noreply.github.com>
2026-05-24 12:07:56 +0200

    Update main.js

--- main.js
+++ main.js
@@ -12,17 +12,42 @@ const styleLink = document.getElementById("style");
 function styleButton(){
     const elementThemeButton = document.getElementById("theme-button");
     const styleLink = document.getElementById("style");
+    const faviconLink = document.getElementById("favicon")
     console.log("style link value:" + styleLink.getAttribute("href"))
     if(styleLink.getAttribute("href") == 'cute.css'){
         console.log("style = cute.css")
         styleLink.setAttribute("href", "normal.css");
+        audio("normal")
+        faviconLink.setAttribute("href", "favicon.png")
     } else {
         console.log("style = normal.css")
         styleLink.setAttribute("href", "cute.css");
+        audio('cute')
+        faviconLink.setAttribute("href", "favicon-cute.png")
     }
     buttonClick()
 }
 
+    let cuteAudio = new Audio('cute.mp3');
+    let normalAudio = new Audio('normal.mp3');
+
+function audio(type){
+    cuteAudio.pause()
+    cuteAudio.currentTime = 0;
+    normalAudio.pause()
+    normalAudio.currentTime = 0;
+
+    if(type == "cute"){
+    cuteAudio.play();
+    cuteAudio.loop = true;
+    console.log("cute audio start")
+    } else {
+    normalAudio.play();
+    normalAudio.loop = true;
+    normalAudio.volume = 0.4;
+    console.log("normal audio start")
+    }
+}
 function buttonClick(){
     let audio = new Audio('click.mp3');
     audio.play();
